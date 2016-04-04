5da0cd1e71d21c4e79acfe181cc3b98266ce1653
Student <Student@10.2.3.3>
2016-04-04 15:09:06 +0200

    Commit z branchem weighten_quick_union

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -32,4 +32,19 @@ public class UF {
 		 }
 	}
 	
+	private int root(int i) {
+		 while (i != id[i]) {
+		 i = id[i];
+		 }
+		 return i;
+		}
+		public boolean connected(int p, int q) {
+		 return root(p) == root(q);
+		}
+		public void union(int p, int q) {
+		 int i = root(p);
+		 int j = root(q);
+		 id[i] = j;
+		}
+	
 }
