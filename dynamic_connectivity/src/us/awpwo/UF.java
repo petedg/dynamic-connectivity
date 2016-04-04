bcace5449c530f16985e1a322bd1fccfb21c7b8e
Student <Student@10.2.3.3>
2016-04-04 15:07:24 +0200

    Commit z branchem quick_union

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -18,4 +18,18 @@ public class UF {
 		return false;
 	}
 	
+	public boolean connected(int p, int q) {
+		return id[p] == id[q];
+	}
+	
+	public void union(int p, int q) {
+		 int pid = id[p];
+		 int qid = id[q];
+		 for (int i = 0; i < id.length; ++i) {
+			 if (id[i] == pid) {
+				 id[i] = qid;
+			 }
+		 }
+	}
+	
 }
