11c210cd021968048749750842e6aea1bd34a100
Student <Student@10.2.3.3>
2016-04-04 15:11:38 +0200

    Commit

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -3,12 +3,15 @@ package us.awpwo;
 public class UF {
 
 	protected int[] id;
+	private int [] sz;
 	
 	public UF(int N) {
 		id = new int[N];
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
 		}
+		
+		sz = new int[N];
 	}
 	
 	public void union(int p, int q) {
@@ -42,9 +45,17 @@ public class UF {
 		 return root(p) == root(q);
 		}
 		public void union(int p, int q) {
-		 int i = root(p);
-		 int j = root(q);
-		 id[i] = j;
+			int i = root(p);
+			int j = root(q);
+			if (i == j) return;
+			 if (sz[i] < sz[j]) {
+			 id[i] = j;
+			 sz[j] += sz[i];
+			 }
+			else {
+			 id[j] = i;
+			 sz[i] += sz[j];
+			}	 
 		}
 	
 }
